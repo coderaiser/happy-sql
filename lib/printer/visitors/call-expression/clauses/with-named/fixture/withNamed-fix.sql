3a39fbfa705d194025ec52c697bfcee5637fe86c
coderaiser <coderaiser@cloudcmd.io>
2026-08-13 15:22:10 +0000

    feature: with: improve

---
+++ lib/printer/visitors/call-expression/clauses/with-named/fixture/withNamed-fix.sql
@@ -0,0 +1,10 @@
+WITH
+    call AS (
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id) RETURNING id
+    ),
+    member AS (
+        INSERT INTO MemberExpression (parent_id) SELECT id
+FROM call RETURNING id
+    )
+INSERT INTO Identifier (name, parent_id) SELECT 'x', id
+FROM member
