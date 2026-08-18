8ae76bcb4ab93b76c99188245ad65dc660cb9e9e
coderaiser <coderaiser@cloudcmd.io>
2026-08-18 21:41:59 +0000

    test: happy-sql: withNamed INSERT round-trip spec

---
+++ lib/printer/visitors/call-expression/clauses/with-named/fixture/with-named-insert-fix.sql
@@ -0,0 +1,10 @@
+WITH
+    call AS (
+        INSERT INTO CallExpression (file, parent_id) VALUES (':file', ':parent_id') RETURNING id
+    ),
+    member AS (
+        INSERT INTO MemberExpression (file, parent_id) SELECT :file, id
+FROM call RETURNING id
+    )
+INSERT INTO Identifier (file, name, parent_id) SELECT :file, 'isNaN', id
+FROM member
