4a5ddb3828424242cd154068d9fd1978a97b0c3d
coderaiser <coderaiser@users.noreply.github.com>
2026-08-20 13:55:03 +0000

    chore: happy-sql: actions: lint ☘️

--- test/happy-sql.js
+++ test/happy-sql.js
@@ -1,9 +1,12 @@
 import {parseSql, printSql} from 'happy-sql';
 import {test} from 'supertape';
+import {montag} from 'montag';
 
 test('happy-sql: roundtrip: select star', (t) => {
-    const source = `SELECT *
-FROM users`;
+    const source = montag`
+        SELECT *
+        FROM users
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -14,8 +17,10 @@ FROM users`;
 });
 
 test('happy-sql: roundtrip: select columns', (t) => {
-    const source = `SELECT id, name
-FROM users`;
+    const source = montag`
+        SELECT id, name
+        FROM users
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -26,8 +31,10 @@ FROM users`;
 });
 
 test('happy-sql: roundtrip: select alias', (t) => {
-    const source = `SELECT 'Prefer let over const' AS message, start_line AS line
-FROM VariableDeclaration`;
+    const source = montag`
+        SELECT 'Prefer let over const' AS message, start_line AS line
+        FROM VariableDeclaration
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -38,9 +45,11 @@ FROM VariableDeclaration`;
 });
 
 test('happy-sql: roundtrip: where param', (t) => {
-    const source = `SELECT *
-FROM users
-WHERE id = :id`;
+    const source = montag`
+        SELECT *
+        FROM users
+        WHERE id = :id
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -51,10 +60,12 @@ WHERE id = :id`;
 });
 
 test('happy-sql: roundtrip: where and', (t) => {
-    const source = `SELECT *
-FROM users
-WHERE file = :file
-AND kind = 'const'`;
+    const source = montag`
+        SELECT *
+        FROM users
+        WHERE file = :file
+        AND kind = 'const'
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -65,24 +76,31 @@ AND kind = 'const'`;
 });
 
 test('happy-sql: roundtrip: where or', (t) => {
-    const source = `SELECT *
-FROM users
-WHERE x = ':x' OR y = ':y'`;
+    const source = montag`
+        SELECT *
+        FROM users
+        WHERE x = ':x' OR y = ':y'
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
     
-    const expected = `SELECT *
-FROM users
-WHERE x = :x
-OR y = :y\n`;
+    const expected = montag`
+        SELECT *
+        FROM users
+        WHERE x = :x
+        OR y = :y
+    
+    `;
     
     t.equal(result, expected);
     t.end();
 });
 
 test('happy-sql: roundtrip: insert single', (t) => {
-    const source = `INSERT INTO CallExpression (parent_id) VALUES (:parent_id)`;
+    const source = montag`
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -93,7 +111,9 @@ test('happy-sql: roundtrip: insert single', (t) => {
 });
 
 test('happy-sql: roundtrip: insert multi', (t) => {
-    const source = `INSERT INTO CallExpression (parent_id, parent_type) VALUES (:parent_id, :parent_type)`;
+    const source = montag`
+        INSERT INTO CallExpression (parent_id, parent_type) VALUES (:parent_id, :parent_type)
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -104,8 +124,10 @@ test('happy-sql: roundtrip: insert multi', (t) => {
 });
 
 test('happy-sql: roundtrip: on conflict nothing', (t) => {
-    const source = `INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
-ON CONFLICT DO NOTHING`;
+    const source = montag`
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
+        ON CONFLICT DO NOTHING
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -116,8 +138,10 @@ ON CONFLICT DO NOTHING`;
 });
 
 test('happy-sql: roundtrip: on conflict target nothing', (t) => {
-    const source = `INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
-ON CONFLICT (parent_id) DO NOTHING`;
+    const source = montag`
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
+        ON CONFLICT (parent_id) DO NOTHING
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -128,23 +152,30 @@ ON CONFLICT (parent_id) DO NOTHING`;
 });
 
 test('happy-sql: roundtrip: on conflict update', (t) => {
-    const source = `INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
-ON CONFLICT (parent_id) DO UPDATE SET parent_type = ':parent_type'`;
+    const source = montag`
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
+        ON CONFLICT (parent_id) DO UPDATE SET parent_type = ':parent_type'
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
     
-    const expected = `INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
-ON CONFLICT (parent_id) DO UPDATE SET parent_type = :parent_type\n`;
+    const expected = montag`
+        INSERT INTO CallExpression (parent_id) VALUES (:parent_id)
+        ON CONFLICT (parent_id) DO UPDATE SET parent_type = :parent_type
+    
+    `;
     
     t.equal(result, expected);
     t.end();
 });
 
 test('happy-sql: roundtrip: update', (t) => {
-    const source = `UPDATE CallExpression
-SET parent_type = :parent_type
-WHERE parent_id = :parent_id`;
+    const source = montag`
+        UPDATE CallExpression
+        SET parent_type = :parent_type
+        WHERE parent_id = :parent_id
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -155,8 +186,10 @@ WHERE parent_id = :parent_id`;
 });
 
 test('happy-sql: roundtrip: delete', (t) => {
-    const source = `DELETE FROM CallExpression
-WHERE parent_id = :parent_id`;
+    const source = montag`
+        DELETE FROM CallExpression
+        WHERE parent_id = :parent_id
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -167,9 +200,11 @@ WHERE parent_id = :parent_id`;
 });
 
 test('happy-sql: roundtrip: section', (t) => {
-    const source = `-- @select
-SELECT *
-FROM users;`;
+    const source = montag`
+        -- @select
+        SELECT *
+        FROM users;
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -180,29 +215,37 @@ FROM users;`;
 });
 
 test('happy-sql: roundtrip: section multi', (t) => {
-    const source = `-- @select
-SELECT *
-FROM users;
--- @report
-SELECT 'test' AS message\n`;
+    const source = montag`
+        -- @select
+        SELECT *
+        FROM users;
+        -- @report
+        SELECT 'test' AS message
+    
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
     
-    const expected = `-- @select
-SELECT *
-FROM users;
-
--- @report
-SELECT 'test' AS message;\n`;
+    const expected = montag`
+        -- @select
+        SELECT *
+        FROM users;
+        
+        -- @report
+        SELECT 'test' AS message;
+    
+    `;
     
     t.equal(result, expected);
     t.end();
 });
 
 test('happy-sql: roundtrip: create table autoincrement', (t) => {
-    const source = `CREATE TABLE test (
-id SERIAL PRIMARY KEY)`;
+    const source = montag`
+        CREATE TABLE test (
+        id SERIAL PRIMARY KEY)
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -213,9 +256,11 @@ id SERIAL PRIMARY KEY)`;
 });
 
 test('happy-sql: roundtrip: create table with multiple columns', (t) => {
-    const source = `CREATE TABLE test (
-id INTEGER,
-name TEXT)`;
+    const source = montag`
+        CREATE TABLE test (
+        id INTEGER,
+        name TEXT)
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
@@ -226,8 +271,10 @@ name TEXT)`;
 });
 
 test('happy-sql: roundtrip: create table serial', (t) => {
-    const source = `CREATE TABLE test (
-id SERIAL)`;
+    const source = montag`
+        CREATE TABLE test (
+        id SERIAL)
+    `;
     
     const ast = parseSql(source);
     const result = printSql(ast);
