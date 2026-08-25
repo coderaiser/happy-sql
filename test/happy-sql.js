196eae8eaa770f6b9684b5e29d08109d20e89c6a
coderaiser <coderaiser@cloudcmd.io>
2026-08-25 10:08:35 +0000

    fix: drop-index: IF EXISTS support

--- test/happy-sql.js
+++ test/happy-sql.js
@@ -1533,3 +1533,16 @@ test('happy-sql: roundtrip: create index where', (t) => {
     t.equal(result, expected);
     t.end();
 });
+
+test('happy-sql: roundtrip: drop index if exists', (t) => {
+    const source = montag`
+        DROP INDEX IF EXISTS idx
+    `;
+    
+    const ast = parseSql(source);
+    const result = printSql(ast);
+    const expected = `${source}\n`;
+    
+    t.equal(result, expected);
+    t.end();
+});
