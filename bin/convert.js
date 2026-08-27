3fff1e3c88a5435da6454456388a7d9ec2a10ae8
coderiaser <coderaiser@cloudcmd.io>
2026-08-27 22:21:10 +0300

    feature: happy-sql: putout_processor_sql

--- bin/convert.js
+++ bin/convert.js
@@ -1,15 +1,9 @@
-import {
-    __markdown_name,
-    fromJS,
-} from '@putout/operator-json';
+import {__sql_name, fromJS} from '@putout/operator-json';
 import {convertJsToSql, convertSqlToJs} from '#happy-sql';
 
 export function convert(source) {
-    if (source.startsWith(__markdown_name))
-        return convertJsToSql(fromJS(
-            source,
-            __markdown_name,
-        ));
+    if (source.startsWith(__sql_name))
+        return convertJsToSql(fromJS(source, __sql_name));
     
     if (source.startsWith('['))
         return convertJsToSql(source);
--- bin/convert.spec.js
+++ bin/convert.spec.js
@@ -1,9 +1,6 @@
 import {test} from 'supertape';
 import {montag} from 'montag';
-import {
-    __markdown_name,
-    toJS,
-} from '@putout/operator-json';
+import {__sql_name, toJS} from '@putout/operator-json';
 import {convert} from './convert.js';
 
 test('happy-sql: bin: convert: js: [', (t) => {
@@ -18,23 +15,22 @@ test('happy-sql: bin: convert: js: [', (t) => {
     t.end();
 });
 
-test('happy-sql: bin: convert: js: __putout_processor_markdown', (t) => {
-    const js = toJS(`[select('*', from(users))]`, __markdown_name);
-    
+test('happy-sql: bin: convert: js: __putout_processor_sql', (t) => {
+    const js = toJS(`[select('*', from(users))]`, __sql_name);
     const result = convert(js);
+    
     const expected = montag`
         SELECT *
         FROM users
-    ` +
-        '\n';
+    
+    `;
     
     t.equal(result, expected);
     t.end();
 });
 
-test('happy-sql: bin: convert: markdown: __putout_processor_markdown', (t) => {
+test('happy-sql: bin: convert: __putout_processor_sql', (t) => {
     const source = 'SELECT * FROM users';
-    
     const result = convert(source);
     
     const expected = montag`
